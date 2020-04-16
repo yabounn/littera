a6fe5f888d18143e4c5db188e07aa337b367e69b
yabounn <bcd.yann@gmail.com>
2020-04-16 22:21:57 +0200

    Ajout dépendances autoloader, router, debug

--- public/index.php
+++ public/index.php
@@ -1,2 +1,11 @@
-Les routes!
- 
+<?php 
+require '../vendor/autoload.php';
+
+$router = new AltoRouter();
+
+$router->map('GET', '/', function(){
+    require dirname(__DIR__) . '/views/home/index.php';
+});
+
+$match = $router->match();
+$match['target']();
--- views/home/index.php
+++ views/home/index.php
@@ -1,1 +1,5 @@
+<?php 
+require '../views/partials/header.php'; 
+?>
 <h1>Homepage littera</h1>
+<?php require '../views/partials/footer.php' ?>
