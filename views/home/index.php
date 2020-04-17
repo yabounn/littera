542153f799a66625bbd69ecb4122a09161e062e9
yabounn <bcd.yann@gmail.com>
2020-04-17 14:41:51 +0200

    Ajout navbar dans le header + début css bootstrap

--- views/home/index.php
+++ views/home/index.php
@@ -1,1 +1,3 @@
+<?php require '../views/partials/header.php' ?>
 <h1>Homepage littera</h1>
+<?php require '../views/partials/footer.php' ?>
--- views/partials/header.php
+++ views/partials/header.php
@@ -1,12 +1,32 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="fr" class="h-100">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>Document</title>
 </head>
-<body>
-    <div class="container mt-4">
 
+<body class="d-flex flex-column h-100">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarToggler">
+            <a class="navbar-brand" href="#">Littera</a>
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Librairie</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Forum</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
     
+    <div class="container mt-4">
