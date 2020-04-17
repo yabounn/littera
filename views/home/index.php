d0d18e31e1516be4a67dc800e1433bf7549e83af
yabounn <bcd.yann@gmail.com>
2020-04-17 22:27:32 +0200

    Mise en page de la homepage

--- views/home/index.php
+++ views/home/index.php
@@ -1,3 +1,224 @@
 <?php require '../views/partials/header.php' ?>
-<h1>Homepage littera</h1>
+<div class="row col-md-12 homepage">
+    <div class="col-md-8">
+        <div class="row carousel-set">
+            <div class=" col-md-12 section-carousel">
+                <h3>En ce moment ...</h3>
+                <div class="section-preview text-center">
+                    <div id="option" class="carousel slide" data-ride="carousel">
+                        <ul class="carousel-indicators">
+                            <li data-target="#option" data-slide-to="0" class="active"></li>
+                            <li data-target="#option" data-slide-to="1"></li>
+                            <li data-target="#option" data-slide-to="2"></li>
+                            <li data-target="#option" data-slide-to="3"></li>
+                            <li data-target="#option" data-slide-to="4"></li>
+                            <li data-target="#option" data-slide-to="5"></li>
+                        </ul>
+                        <!-- /.indicators pagination -->
+                        <div class="controls-top">
+                            <a class="btn-floating ripple" href="#option" data-slide="prev"><i class="fas fa-chevron-left fa-2x"></i></a>
+                            <a class="btn-floating ripple" href="#option" data-slide="next"><i class="fas fa-chevron-right fa-2x"></i></a>
+                        </div>
+                        <!--/.Controls-->
+                        <!-- slides -->
+                        <div class="carousel-inner" role="listbox">
+                            <div class="carousel-item active">
+                                <div class="col-12 col-md-4">
+                                    <div class="card mb-2">
+                                        <img class="card-img-top" src="images/ancient-books.jpg" alt="Livre antique">
+                                        <div class="card-body">
+                                            <h4 class="card-title font-weight-bold">Titre du livre 1</h4>
+                                            <h4 class="card-text">Auteur</h4>
+                                            <h5 class="card-text">Categorie</h5>
+                                            <p class="card-text">Lorem ipsum dolor sit amet consectetur
+                                                adipisicing
+                                                elit.
+                                                Distinctio
+                                                nobis asperiores dolores.</p>
+                                            <a href="#" class="btn btn-primary btn-md btn-rounded ">Plus</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="carousel-item">
+                                <div class="col-12 col-md-4">
+                                    <div class="card mb-2">
+                                        <img class="card-img-top" src="images/ancient-books.jpg" alt="Livre antique">
+                                        <div class="card-body">
+                                            <h4 class="card-title font-weight-bold">Titre du livre 2</h4>
+                                            <h4 class="card-text">Auteur</h4>
+                                            <h5 class="card-text">Categorie</h5>
+                                            <p class="card-text">Lorem ipsum dolor sit amet consectetur
+                                                adipisicing
+                                                elit.
+                                                Distinctio
+                                                nobis asperiores dolores.</p>
+                                            <a href="#" class="btn btn-primary btn-md btn-rounded ">Plus</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="carousel-item">
+                                <div class="col-12 col-md-4">
+                                    <div class="card mb-2">
+                                        <img class="card-img-top" src="images/ancient-books.jpg" alt="Livre ouvert">
+                                        <div class="card-body">
+                                            <h4 class="card-title font-weight-bold">Titre du livre 3</h4>
+                                            <h4 class="card-text">Auteur</h4>
+                                            <h5 class="card-text">Categorie</h5>
+                                            <p class="card-text">Lorem ipsum dolor sit amet consectetur
+                                                adipisicing
+                                                elit.
+                                                Distinctio
+                                                nobis asperiores dolores.</p>
+                                            <a href="#" class="btn btn-primary btn-md btn-rounded ">Plus</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="carousel-item">
+                                <div class="col-12 col-md-4">
+                                    <div class="card mb-2">
+                                        <img class="card-img-top" src="images/ancient-books.jpg" alt="Livre antique">
+                                        <div class="card-body">
+                                            <h4 class="card-title font-weight-bold">Titre du livre 4</h4>
+                                            <h4 class="card-text">Auteur</h4>
+                                            <h5 class="card-text">Categorie</h5>
+                                            <p class="card-text">Lorem ipsum dolor sit amet consectetur
+                                                adipisicing
+                                                elit.
+                                                Distinctio
+                                                nobis asperiores dolores.</p>
+                                            <a href="#" class="btn btn-primary btn-md btn-rounded ">Plus</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="carousel-item">
+                                <div class="col-12 col-md-4">
+                                    <div class="card mb-2">
+                                        <img class="card-img-top" src="images/book-pages.jpg" alt="Livre ouvert">
+                                        <div class="card-body">
+                                            <h4 class="card-title font-weight-bold">Titre du livre 5</h4>
+                                            <h4 class="card-text">Auteur</h4>
+                                            <h5 class="card-text">Categorie</h5>
+                                            <p class="card-text">Lorem ipsum dolor sit amet consectetur
+                                                adipisicing
+                                                elit.
+                                                Distinctio
+                                                nobis asperiores dolores.</p>
+                                            <a href="#" class="btn btn-primary btn-md btn-rounded ">Plus</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="carousel-item">
+                                <div class="col-12 col-md-4">
+                                    <div class="card mb-2">
+                                        <img class="card-img-top" src="images/ancient-books.jpg" alt="Livre antique">
+                                        <div class="card-body">
+                                            <h4 class="card-title font-weight-bold">Titre du livre 6</h4>
+                                            <h4 class="card-text">Auteur</h4>
+                                            <h5 class="card-text">Categorie</h5>
+                                            <p class="card-text">Lorem ipsum dolor sit amet consectetur
+                                                adipisicing
+                                                elit.
+                                                Distinctio
+                                                nobis asperiores dolores.</p>
+                                            <a href="#" class="btn btn-primary btn-md btn-rounded ">Plus</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.slides -->
+                    </div>
+                    <!-- /.carousel content -->
+                </div>
+                <!-- /.section-preview -->
+            </div>
+            <!-- /.section-carousel -->
+            <div class="col-md-12 book-selection ">
+                <h3>Sélection du livre de la semaine</h3>
+                <div class="card-selection">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="images/ancient-books.jpg" class="card-img" alt="Livre antique">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Titre du livre</h5>
+                                <h5 class="card-title">Auteur</h5>
+                                <h6 class="card-title">Catégorie</h6>
+                                <p class="card-text">This is a wider card with supporting text below as a
+                                    natural lead-in to additional content. This content is a little bit longer.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--/.card-selection-->
+            </div>
+            <!--/.book-selection-->
+        </div>
+        <!--/.carousel-set-->
+    </div>
+
+    <div class="col-md-4 best-selling">
+        <div class="row book-comment">
+            <div class="col-md-12 best-book">
+                <div class="jumbotron">
+                    <h4>Meilleure vente</h4>
+                    <div class="card mb-2">
+                        <img class="card-img-top" src="images/ancient-books.jpg" alt="Livre antique">
+                        <div class="card-body">
+                            <h4 class="card-title font-weight-bold">Titre du livre 2</h4>
+                            <h4 class="card-text">Auteur</h4>
+                            <h5 class="card-text">Categorie</h5>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.jumbotron -->
+            </div>
+            <!-- /.best-book -->
+            <div class="col-md-12 last-comment">
+                <div class="jumbotron">
+                    <h4 class="talk-about">On en parle</h4>
+                    <div class="card">
+                        <a href="#">
+                            <div class="card-header">
+                                <h5>Titre du livre 1</h5>
+                            </div>
+                        </a>
+                        <a href="#"></a>
+                        <div class="card-content">
+                            <p class="card-author">Jean</p>
+                            <p>a laissé ce commentaire :</p>
+                            <p class="card-content"><span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum exercitationem atque, nemo corrupti in blanditiis voluptatum? Ipsum, fugiat dolore possimus sed delectus quisquam vel atque vitae harum. Blanditiis, ratione hic!</span></p>
+                        </div>
+                        <div class="card-action">
+                            <ul class="tools">
+                                <li><i class="fas fa-heart"></i><span class="tools-item-counter">14</span></li>
+                                <li><i class="fas fa-comment"></i><span class="tools-item-counter">5</span></li>
+                                <li><i class="fas fa-eye"></i><span class="tools-item-counter">230</span></li>
+                            </ul>
+                        </div>
+                        </a>
+                    </div>
+                    <!--/.card-->
+                </div>
+                <!-- /.jumbotron -->
+            </div>
+            <!--/.last-comment -->
+        </div>
+        <!--/.grid row book-comment-->
+    </div>
+    <!--/.best-selling-->
+</div>
+<!--/.grid row homepage -->
 <?php require '../views/partials/footer.php' ?>
