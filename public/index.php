8155b172b505507dd7a76b88203edfda9934a89a
yabounn <bcd.yann@gmail.com>
2020-04-17 13:54:13 +0200

    Création de la route pour la Homepage

--- public/index.php
+++ public/index.php
@@ -1,2 +1,13 @@
-Les routes!
- 
+<?php
+require '../vendor/autoload.php';
+
+$router = new AltoRouter();
+
+$router->map('GET', '/', function(){
+    require '../views/home/index.php';
+}, 'home');
+
+$match = $router->match();
+// var_dump($match);
+// exit();
+$match["target"]();
