ccf6a6caab77021bedc2bafcec0d18fc7df2fb54
yabounn <bcd.yann@gmail.com>
2020-04-19 00:17:13 +0200

    style:positionnement livres dans bookstore

--- views/bookstore/index.php
+++ views/bookstore/index.php
@@ -38,6 +38,78 @@
             </div>
             <!--/.book-view .book-closeview-->
         </div>
+        <div class="col-md-3 col-sm-6 book-container">
+            <h5 class="store-book-title">
+                <a href="#">Titre du livre</a>
+            </h5>
+            <div class="book-view book-closeview">
+                <div class="cover-img">
+                    <div class="front">
+                        <div class="thumbnail-img">
+                            <img class="card-img" src="images/ancient-books.jpg" alt="extra-danse">
+                        </div>
+                    </div>
+                    <!--/.front-->
+                    <div class="cover"></div>
+                    <div class="back">
+                        <h6>Titre du livre</h6>
+                        <h6>Auteur</h6>
+                        <p>Prix (€) : </p>
+                        <a href="#" class="btn blue-gradient btn-rounded">Plus de détails</a>
+                    </div>
+                </div>
+                <!--/.cover-img-->
+            </div>
+            <!--/.book-view .book-closeview-->
+        </div>
+        <div class="col-md-3 col-sm-6 book-container">
+            <h5 class="store-book-title">
+                <a href="#">Titre du livre</a>
+            </h5>
+            <div class="book-view book-closeview">
+                <div class="cover-img">
+                    <div class="front">
+                        <div class="thumbnail-img">
+                            <img class="card-img" src="images/LaTex.jpg" alt="extra-danse">
+                        </div>
+                    </div>
+                    <!--/.front-->
+                    <div class="cover"></div>
+                    <div class="back">
+                        <h6>Titre du livre</h6>
+                        <h6>Auteur</h6>
+                        <p>Prix (€) : </p>
+                        <a href="#" class="btn blue-gradient btn-rounded">Plus de détails</a>
+                    </div>
+                </div>
+                <!--/.cover-img-->
+            </div>
+            <!--/.book-view .book-closeview-->
+        </div>
+        <div class="col-md-3 col-sm-6 book-container">
+            <h5 class="store-book-title">
+                <a href="#">Titre du livre</a>
+            </h5>
+            <div class="book-view book-closeview">
+                <div class="cover-img">
+                    <div class="front">
+                        <div class="thumbnail-img">
+                            <img class="card-img" src="images/php-mysql-nuls.jpg" alt="extra-danse">
+                        </div>
+                    </div>
+                    <!--/.front-->
+                    <div class="cover"></div>
+                    <div class="back">
+                        <h6>Titre du livre</h6>
+                        <h6>Auteur</h6>
+                        <p>Prix (€) : </p>
+                        <a href="#" class="btn blue-gradient btn-rounded">Plus de détails</a>
+                    </div>
+                </div>
+                <!--/.cover-img-->
+            </div>
+            <!--/.book-view .book-closeview-->
+        </div>
     </div>
 </div>
 <?php require '../views/partials/footer.php' ?>
